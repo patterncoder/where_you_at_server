1028b3416d6eeaf2b9ec1e751771bcaa48ee9c9f
patterncoder <chris@oldtowndining.com>
2014-08-07 17:21:00 -0700

    almost there

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //var http = require('http')
-var port = process.env.PORT || 1337;
+//var port = process.env.PORT || 1337;
 //http.createServer(function (req, res) {
 //    res.writeHead(200, { 'Content-Type': 'text/plain' });
 //    res.end('Hello World\n');
@@ -13,19 +13,19 @@ var express = require('express');
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var app = express();
 //var config = require('./config/config');
 var config = require('./config/config')[env];
-//require('./mongoose')(config);
-//var routes = require('./routes');
+require('./mongoose')(config);
+var routes = require('./routes');
 //console.log(config.db)
 //// all environments
 //app.set('port', process.env.PORT || 3000);
 
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 ////app.use(express.static(path.join(__dirname, 'public')));
 
 //// development only
@@ -33,11 +33,11 @@ var config = require('./config/config')[env];
 ////  app.use(express.errorHandler());
 ////}
 
-////app.get('/', routes.index);
-app.get('/', function (req, res) {
-    res.send("hello");
-});
+app.get('/', routes.index);
+//app.get('/', function (req, res) {
+//    res.send("hello");
+//});
 
 
-app.listen(port);
+app.listen(config.port);
 console.log('Express server listening on port ' + config.port);
