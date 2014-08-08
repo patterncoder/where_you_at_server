d49d527ff026a60569548df369d2b9ef6d1ac291
patterncoder <chris@oldtowndining.com>
2014-08-07 17:09:03 -0700

    Alex git ignored config.js

---
+++ config/config.js
@@ -0,0 +1,21 @@
+﻿//var path = require('path');
+//var rootPath = path.normalize(__dirname + '/');
+
+//module.exports = {
+//    development: {
+//        rootPath: rootPath
+//        , db: 'mongodb://localhost/whereyouat2'
+//        , port: process.env.PORT || 3000
+//    },
+//    production: {
+//        rootPath: rootPath
+//        , db: 'mongodb://whereyouat2:tvsd_20!4@ds050087.mongolab.com:50087/whereyouat2'
+//        , port: process.env.PORT || 80
+//    }
+
+
+
+//    // use below command to connect mongo shell to mongolab
+//    // mongo ds030607.mongolab.com:30607/tipminer -u patterncoder -p y5EQJ5m7C3
+
+//}
